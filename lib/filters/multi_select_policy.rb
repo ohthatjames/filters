0c7b12f3c4afa44c98c1291c0b3fad686f85767d
James Hunt <ohthatjames@gmail.com>
2012-12-09 19:13:18 +0000

    Remove unused method

--- lib/filters/multi_select_policy.rb
+++ lib/filters/multi_select_policy.rb
@@ -1,9 +1,5 @@
 module Filters
   class MultiSelectPolicy
-    def multi_select_allowed?
-      true
-    end
-
     def selected_values_from(value)
       value.split(',')
     end
--- lib/filters/single_select_policy.rb
+++ lib/filters/single_select_policy.rb
@@ -1,9 +1,5 @@
 module Filters
   class SingleSelectPolicy
-    def multi_select_allowed?
-      false
-    end
-
     def selected_values_from(value)
       value.empty? ? [] : [value]
     end
